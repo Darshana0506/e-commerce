998c8d097f087bee5cff3646bcc3d3cddd684ed8
darshanamohanan204@gmail.com <darshanamohanan204@gmail.com>
2024-05-11 04:24:21 +0000

    Created Mycourses; Edited settings, base, Courses and Home

--- client_code/Mycourses/__init__.py
+++ client_code/Mycourses/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import HomeTemplate
+from ._anvil_designer import MycoursesTemplate
 from anvil import *
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
@@ -6,17 +6,11 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.users
-from ..Courses import Courses
 
 
-class Home(HomeTemplate):
+class Mycourses(MycoursesTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-  def view_courses_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    self.content_panel.clear()
-    self.content_panel.add_component(Courses())
